d57174dcfa901d6571630b1dfd0bd031b6da9284
EugenioBravi <bravieugenio@gmail.com>
2023-07-12 17:39:40 -0300

    Corrijo error en obtener la sumaVerificacion

--- descargar.sh
+++ descargar.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/bash
 
 [ ! $# -eq 2 ] && exit 1
+wget "$1" -O ./"imagenes_Generadas.zip"
+SUMAVERIFICACION=$(wget "$2" -O ./"sumaVerificacion" |cat sumaVerificacion | cut -d " " -f "1")
+SUMAIMAGENES=$(sha256sum ./"imagenes_Generadas.zip" | cut -d " " -f "1")
 
-IMAGENES="$1"
-SUMAVERIFICACION="$2"
-SUMAIMAGENES="$(sha256sum "$IMAGENES" | cut -d " " -f 1)"
-
-if [ "$SUMAIMAGENES" == "$(cat $SUMAVERIFICACION)" ]; then
-    unzip "$IMAGENES"
-    echo "Las imagenes se descomprimido con exito"
+if [ "$SUMAIMAGENES" == "$SUMAVERIFICACION" ]; then
+    echo "La suma verificacion es correcta"
     exit 0
 else
-    echo "La suma de verificacion es incorrecta"
+    echo "$SUMAIMAGENES"
+    echo "$SUMAVERIFICACION"
+    echo "La suma verificacion es incorrecta"
     exit 2
 fi
