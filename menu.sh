2a72e786649bf35d50a31b9d1358f362e5b3f628
EugenioBravi <bravieugenio@gmail.com>
2023-07-12 20:45:56 -0300

    Agrego espacios

--- menu.sh
+++ menu.sh
@@ -19,8 +19,8 @@ while [[ $opcion =~ [1-9] ]]; do
             bash generar.sh $numero
             ;;
         2)
-            read -p "ingrese la ruta del archivo" archivo
-            read -p "ingrese la ruta del archivo de suma de verificacion" suma_verificacion
+            read -p "ingrese la ruta del archivo: " archivo
+            read -p "ingrese la ruta del archivo de suma de verificacion: " suma_verificacion
             bash descargar.sh $archivo $suma_verificacion
             ;;
         3)
