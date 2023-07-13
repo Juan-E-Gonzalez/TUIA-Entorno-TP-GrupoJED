41c33647228a26dd61de0b507ee768e9e4167bec
EugenioBravi <bravieugenio@gmail.com>
2023-07-12 21:49:52 -0300

    Correcion error de sintaxis

--- descargar.sh
+++ descargar.sh
@@ -2,15 +2,15 @@
 
 [ ! $# -eq 2 ] && exit 1
 wget "$1" -O ./"imagenes_Generadas.zip"
-SUMAVERIFICACION=$(wget "$2" -O ./"sumaVerificacion" |cat sumaVerificacion | cut -d " " -f "1")
+SUMAVERIFICACION=$(wget "$2" -O ./sumaVerificacion && cat sumaVerificacion | cut -d " " -f "1")
 SUMAIMAGENES=$(sha256sum ./"imagenes_Generadas.zip" | cut -d " " -f "1")
 
 if [ "$SUMAIMAGENES" == "$SUMAVERIFICACION" ]; then
     echo "La suma verificacion es correcta"
     exit 0
 else
-    echo "$SUMAIMAGENES"
-    echo "$SUMAVERIFICACION"
+    echo "Imagen: $SUMAIMAGENES"
+    echo "SumaV: $SUMAVERIFICACION"
     echo "La suma verificacion es incorrecta"
     exit 2
 fi
